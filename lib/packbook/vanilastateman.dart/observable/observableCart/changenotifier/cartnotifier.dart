65575fadacb5f272d00f1dd9d5f3b33da2e954e2
eoeefosa <eoeefosa@gmail.com>
2023-06-30 01:41:52 +0100

    updated notifier

--- lib/packbook/vanilastateman.dart/observable/observableCart/changenotifier/cartnotifier.dart
+++ lib/packbook/vanilastateman.dart/observable/observableCart/changenotifier/cartnotifier.dart
@@ -1,4 +1,34 @@
-import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+class MainPage extends StatefulWidget {
+  const MainPage({super.key});
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  CartNotifier cartNotifier = CartNotifier();
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
+class CartPage extends StatefulWidget {
+  final CartNotifier cartNotifier;
+  const CartPage({super.key, required this.cartNotifier});
+
+  @override
+  State<CartPage> createState() => _CartPageState();
+}
+
+class _CartPageState extends State<CartPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
 
 class CartNotifier extends ChangeNotifier {
   final Map<String, CartListItem> _items = {};
@@ -8,7 +38,17 @@ class CartNotifier extends ChangeNotifier {
   List<CartListItem> get items => _items.values.toList();
   double get totalPrice => _totalPrice;
   int get totalItems => _totalItems;
+
+  void addToCart(ProductListItem item) {
+    notifyListeners();
+  }
+
+  void removeFromCart(CartListItem item) {
+    notifyListeners();
+  }
 }
+
+class ProductListItem {}
 
 class CartListItem {
   final Product? product;
@@ -22,3 +62,53 @@ class Product {
 
   Product(this.price);
 }
+
+
+
+// class CartNotifier extends ChangeNotifier {
+//   final Map<String, CartListItem> _items = {};
+//   double _totalPrice = 0;
+//   int _totalItems = 0;
+
+//   List<CartListItem> get items => _items.values.toList();
+//   double get totalPrice => _totalPrice;
+//   int get totalItems => _totalItems;
+
+//   void addToCart(ProductListItem item) {
+//     // Check if the item already exists in the cart
+//     if (_items.containsKey(item.id)) {
+//       // Increment the quantity of the existing item
+//       _items[item.id]!.quantity++;
+//     } else {
+//       // Add a new item to the cart with quantity 1
+//       _items[item.id] = CartListItem(
+//         id: item.id,
+//         name: item.name,
+//         price: item.price,
+//         quantity: 1,
+//       );
+//     }
+//     // Update the total price and total items
+//     _totalPrice += item.price;
+//     _totalItems++;
+//     // Notify the listeners
+//     notifyListeners();
+//   }
+
+//   void removeFromCart(CartListItem item) {
+//     // Check if the item exists in the cart
+//     if (_items.containsKey(item.id)) {
+//       // Decrement the quantity of the existing item
+//       _items[item.id]!.quantity--;
+//       // If the quantity becomes zero, remove the item from the cart
+//       if (_items[item.id]!.quantity == 0) {
+//         _items.remove(item.id);
+//       }
+//       // Update the total price and total items
+//       _totalPrice -= item.price;
+//       _totalItems--;
+//       // Notify the listeners
+//       notifyListeners();
+//     }
+//   }
+// }
